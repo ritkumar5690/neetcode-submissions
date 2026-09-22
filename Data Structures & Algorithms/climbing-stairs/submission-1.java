7399aea9174471942d38a7c1f170f93612aeb3b3
ritkumar5690 <ritsharma1008@gmail.com>
2026-09-22 11:43:18 +0530

    Add: climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/climbing-stairs/submission-1.java
@@ -0,0 +1,14 @@
+class Solution {
+    private int count;
+    public int climbStairs(int n) {
+        if(n<=2) return n;
+        int p1 = 1,p2 =2;
+        for(int i =3;i<=n;i++){
+            int c = p1 + p2;
+             p1 = p2; 
+            p2 = c;
+           
+        }
+        return p2;
+    }
+}
